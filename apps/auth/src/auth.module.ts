c14f52110260057211e208c26706830ad9a77766
HyunTaek Oh <oht222@naver.com>
2025-05-15 23:51:22 +0900

    feat: add user schema

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './users/schema/user.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/auth/.env',
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ apps/auth/src/users/schema/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { BaseSchema } from '@app/common';
+import { UserRole } from '../enum/role.enum';
+import { SchemaTypes } from 'mongoose';
+
+@Schema({ versionKey: false })
+export class User extends BaseSchema {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  nickname: string;
+
+  @Prop({ enum: UserRole, type: SchemaTypes.String })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
