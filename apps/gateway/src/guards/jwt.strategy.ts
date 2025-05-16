0ef1bc967f3f6a7947aded161ac8351431900531
HyunTaek Oh <oht222@naver.com>
2025-05-16 23:38:26 +0900

    feat: add jwt strategy token validate

--- apps/gateway/src/guards/jwt.strategy.ts
+++ apps/gateway/src/guards/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
@@ -8,7 +8,21 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
       secretOrKey: configService.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: any) {
+    const now = new Date();
+    const expiredAt = new Date(payload.exp * 1000);
+
+    const isTokenExpired = now > expiredAt;
+
+    if (isTokenExpired) {
+      throw new UnauthorizedException('토큰이 만료되었습니다.');
+    }
+
+    return true;
   }
 }
