6ad5fa761d0f3cfa68e4d41b1957d30ad687212c
HyunTaek Oh <oht222@naver.com>
2025-05-16 16:27:40 +0900

    feat: add dto type validation-pipe

--- apps/auth/src/users/dto/request/join.dto.ts
+++ apps/auth/src/users/dto/request/join.dto.ts
@@ -1,10 +1,22 @@
 import { UserRole } from '../../enum/role.enum';
+import { IsEmail, IsEnum, IsString } from 'class-validator';
 
 export class JoinDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   password: string;
+
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsString()
   nickname: string;
+
+  @IsEnum(UserRole)
   role: UserRole;
 }
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
 
+  app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  const swaggerConfig = new DocumentBuilder()
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .setTitle('event and reward manage api')
+    .setDescription('API documentation for nexon event and reward management')
+    .setVersion('0.0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   await app.listen(process.env.PORT ?? 3000);
 }
