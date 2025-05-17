24e116af146290bdb59884dd5868da5167648c4a
HyunTaek Oh <oht222@naver.com>
2025-05-18 03:47:26 +0900

    refactor: change request status type to enum

--- apps/event/src/reward-request/schema/reward-request.schema.ts
+++ apps/event/src/reward-request/schema/reward-request.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
+import { SchemaTypes, Types } from 'mongoose';
 import { BaseSchema } from '@app/common';
+import { RequestStatus } from '@app/common/enum/request-status.enum';
 
 @Schema({ versionKey: false })
 export class RewardRequest extends BaseSchema {
@@ -10,8 +11,12 @@ export class RewardRequest extends BaseSchema {
   @Prop({ type: Types.ObjectId, ref: 'Event', required: true })
   eventId: Types.ObjectId;
 
-  @Prop({ enum: ['PENDING', 'SUCCESS', 'FAILED'], default: 'PENDING' })
-  status: string;
+  @Prop({
+    enum: RequestStatus,
+    default: RequestStatus.PENDING,
+    type: SchemaTypes.String,
+  })
+  status: RequestStatus;
 
   @Prop()
   failedReason?: string;
