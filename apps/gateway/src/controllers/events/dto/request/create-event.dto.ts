6f0a8948e180f204d591276c0042057b0e663911
HyunTaek Oh <oht222@naver.com>
2025-05-19 02:07:46 +0900

    fix: add metadata property to create-event dto

--- apps/gateway/src/controllers/events/dto/request/create-event.dto.ts
+++ apps/gateway/src/controllers/events/dto/request/create-event.dto.ts
@@ -3,10 +3,13 @@ import {
   IsBoolean,
   IsDate,
   IsNumber,
+  IsObject,
   IsOptional,
   IsString,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { RewardMetadata } from '@app/common/metadata/reward-metadata.type';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 class Condition {
   @IsString()
@@ -22,6 +25,17 @@ class Reward {
 
   @IsNumber()
   amount: number;
+  @IsOptional()
+  @IsObject()
+  @ApiPropertyOptional({
+    type: Object,
+    description: '보상 타입에 따라 달라지는 보상 관련 추가 정보',
+    example: {
+      itemCode: 'ITEM123',
+      expireDate: '2025-05-19',
+    },
+  })
+  metadata?: RewardMetadata;
 }
 
 export class CreateEventDto {
