7fd209deffc9fab09aa8f385a928c4a5a80229c9
HyunTaek Oh <oht222@naver.com>
2025-05-19 22:54:46 +0900

    docs: add auth api docs dto description and example

--- apps/gateway/src/controllers/auth/dto/request/login.dto.ts
+++ apps/gateway/src/controllers/auth/dto/request/login.dto.ts
@@ -1,9 +1,18 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({
+    description: '사용자 이메일',
+    example: 'user1@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: '사용자 비밀번호',
+    example: 'string',
+  })
   @IsString()
   password: string;
 }
--- apps/gateway/src/controllers/auth/dto/response/login-result.dto.ts
+++ apps/gateway/src/controllers/auth/dto/response/login-result.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginResultDto {
+  @ApiProperty({
+    description: '액세스 토큰(만료기간 7일)',
+    example: 'ACCESS_TOKEN',
+  })
   accessToken: string;
+  @ApiProperty({
+    description: '리프레시 토큰(만료기간 14일)',
+    example: 'REFRESH_TOKEN',
+  })
   refreshToken: string;
 }
